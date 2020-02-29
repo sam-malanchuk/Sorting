995fb756bcb55b89058cbbed39c300b7196ba738
unknown <malanchuksam@gmail.com>
2020-02-29 12:56:33 -0500

    added quick_sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,3 +1,28 @@
+def partition(numbers):
+    left = []
+    pivot = numbers[0]
+    right = []
+
+    for val in numbers[1:]:
+        if val <= pivot:
+            left.append(val)
+        else:
+            right.append(val)
+
+    return left, pivot, right
+
+def quick_sort(numbers):
+    # base case
+    if len(numbers) <= 1:
+        return numbers
+
+    # figure out how to split
+    left, pivot, right = partition(numbers)
+
+    # recursive case
+    sorted_array = quick_sort(left) + [pivot] + quick_sort(right)
+    return sorted_array
+
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
